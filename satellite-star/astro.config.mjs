84a15a2d435847ac1dc1ca892505e3503119b496
GoodWitchLalya <72929397+GoodWitchLalya@users.noreply.github.com>
2026-01-29 19:17:22 +0100

    Add custom CSS for splash template and update mods headers

--- satellite-star/astro.config.mjs
+++ satellite-star/astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
     integrations: [
        starlight({
           title: 'Lalyan Documentation',
+          customCss: ['./src/styles/custom.css'],
           defaultLocale: 'en',
           locales: {
               en: { label: 'English', lang: 'en' },
